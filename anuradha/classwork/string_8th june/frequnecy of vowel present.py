b4d2f8be42e97e0310b605c332d3b040de6f55d1
Anuradha Verma <anuradhaverma576@gmail.com>
2026-06-08 15:36:26 +0530

    Update frequnecy of vowel present.py

--- anuradha/classwork/string_8th june/frequnecy of vowel present.py
+++ anuradha/classwork/string_8th june/frequnecy of vowel present.py
@@ -1,34 +1,11 @@
-# Input a sentence from the user
-sentence = input("Enter a sentence: ")
+# wap to find out input a senetnce and display the frequnecy of vowel present in that senetece ignoring the given sentence
 
-# Convert sentence to lowercase
-sentence = sentence.lower()
+s = input("Enter a sentence: ")
 
-# Variables to count vowels
-a = 0
-e = 0
-i = 0
-o = 0
-u = 0
+count = 0
 
-# Check each character
-for ch in sentence:
-    
-    if ch == 'a':
-        a = a + 1
-    elif ch == 'e':
-        e = e + 1
-    elif ch == 'i':
-        i = i + 1
-    elif ch == 'o':
-        o = o + 1
-    elif ch == 'u':
-        u = u + 1
+for ch in s:
+    if ch.lower() in "aeiou":
+        count += 1
 
-# Display frequency of vowels
-print("Frequency of vowels:")
-print("A =", a)
-print("E =", e)
-print("I =", i)
-print("O =", o)
-print("U =", u)
+print("Total vowels =", count)
